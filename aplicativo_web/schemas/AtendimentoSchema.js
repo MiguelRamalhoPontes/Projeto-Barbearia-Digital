de0f1342c22fb4bc529a18651d1bd622cad37cf7
Miguel <pontesm879@gmail.com>
2026-05-19 16:29:08 -0300

    add conflito de horario

--- aplicativo_web/schemas/AtendimentoSchema.js
+++ aplicativo_web/schemas/AtendimentoSchema.js
@@ -25,7 +25,7 @@ Atendimento.init({
         allowNull:false,
     },
     tipoServico: {
-        type: DataTypes.ENUM('Corte de cabelo', 'Barba', 'Sobrancelha', 'Outros'),
+        type: DataTypes.ENUM('Cabelo', 'Barba', 'Sobrancelha', 'Outros'),
         allowNull: false,
     },
     profissional: {
